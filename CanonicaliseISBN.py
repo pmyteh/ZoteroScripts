1ab6541ab859dc4cb8ab5e79d9a27af454b0a885
Tom Nicholls <github@tomnicholls.me.uk>
2016-03-11 17:33:58 +0000

    Update docs

--- CanonicaliseISBN.py
+++ CanonicaliseISBN.py
@@ -13,7 +13,6 @@ print("Getting library data from Zotero")
 nitems =  zot.num_items()
 itemsgen =  zot.makeiter(zot.top(limit=10))
 for batch in tqdm(itemsgen, total=int(nitems/10 + int(bool(nitems % 10)))):
-#    print('Item: %s | Key: %s') % (item['data']['itemType'], item['data']['key'])
     for item in batch:
         try:
             isbn = item['data']['ISBN']
@@ -21,19 +20,26 @@ for batch in tqdm(itemsgen, total=int(nitems/10 + int(bool(nitems % 10)))):
             # Not a book-ish item
             continue
             
+        if not isbn:
+            # No ISBN listed
+            continue
+
         # Transform to canonical (bare) form, then return to standardised
         # form with hyphens.
-        if isbn:
-            canisbn = get_canonical_isbn(isbn.replace(' ', ''))
-            if canisbn:
-                newisbn = mask(canisbn)
-#                print((isbn, canisbn, newisbn))
-            else:
-                print("Error extracting ISBN from "+str(isbn))
-                continue
-            if newisbn != isbn:
-                assert newisbn
-                print("Updating "+str(isbn)+" to "+str(newisbn))
-                item['data']['ISBN'] = newisbn
-                if not zot.update_item(item):
-                    raise Exception("Zotero write failed for "+str(isbn))
+        canisbn = get_canonical_isbn(isbn.replace(' ', ''))
+
+        if not canisbn:
+            # This most likely means that the ISBN given is bogus
+            # (e.g. has a faulty checksum). Some books have bogus
+            # ISBNs printed on them, so they are used for cataloguing
+            # by some libraries despite being formally invalid.
+            print("Error extracting ISBN from "+str(isbn))
+            continue
+
+        newisbn = mask(canisbn)
+        if newisbn != isbn:
+            assert newisbn
+            print("Updating "+str(isbn)+" to "+str(newisbn))
+            item['data']['ISBN'] = newisbn
+            if not zot.update_item(item):
+                raise Exception("Zotero write failed for "+str(isbn))
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ZoteroScripts
 =============
 
-Various scripts for use with Zotero. Best used with something like the ExecuteJS add-on for Firefox.
+Various scripts for use with Zotero.
+
+* `purgeTags.js`: Purge all but a specified set of tags from your library. Use with something like the ExecuteJS add-on for Firefox.
+* `CanonicaliseISBN.py`: Convert all ISBNs in the library to the standard dashed form (and report any bogus ISBNs which can't be automatically converted).
 
 All the snippets here are in the public domain, under the terms of the CC0 dedication, unless stated otherwise.
